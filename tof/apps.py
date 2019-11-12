0ec0d67e91e68bf40de8941fb28d40dad3443d13
MaxST <mstolpasov@gmail.com>
2019-11-12 13:55:32 +0300

    типо надежней

--- tof/apps.py
+++ tof/apps.py
@@ -2,7 +2,7 @@
 # @Author: MaxST
 # @Date:   2019-10-29 10:05:01
 # @Last Modified by:   MaxST
-# @Last Modified time: 2019-11-07 17:28:26
+# @Last Modified time: 2019-11-12 13:54:19
 from django.apps import AppConfig
 from django.db import connection
 from django.db.models import F
@@ -29,12 +29,8 @@ class TofConfig(AppConfig):
         except Exception:
             return
 
-        prev = None
         for ct in ContentType.objects.filter(translatablefields__isnull=False).annotate(attr=F('translatablefields__name')):
-            if prev != ct:
-                prev = ct
-                cls = ct.model_class()
-                if issubclass(cls, TranslationsFieldsMixin):
-                    continue
+            cls = ct.model_class()
+            if not issubclass(cls, TranslationsFieldsMixin):
                 cls.__bases__ = (TranslationsFieldsMixin, ) + cls.__bases__
             cls._add_deferred_translated_field(ct.attr)
