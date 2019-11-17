21262abba491dae36f12b1157ffa46c5295bddfc
MaxST <mstolpasov@gmail.com>
2019-11-17 14:42:52 +0300

    Fix #1  - select2 for field name

--- tof/admin.py
+++ tof/admin.py
@@ -2,15 +2,14 @@
 # @Author: MaxST
 # @Date:   2019-10-28 12:30:45
 # @Last Modified by:   MaxST
-# @Last Modified time: 2019-11-12 21:15:47
+# @Last Modified time: 2019-11-17 14:33:00
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
 from django.http import Http404, JsonResponse
 from django.urls import reverse
 
-from .forms import TranslationsForm, TranslatableFieldsForm
+from .forms import TranslatableFieldsForm, TranslationsForm
 from .models import Language, TranslatableFields, Translations
-from django.contrib.contenttypes.models import ContentType
-
 
 
 @admin.register(Language)
@@ -26,20 +25,16 @@ class ContentTypeAdmin(admin.ModelAdmin):
 @admin.register(TranslatableFields)
 class AdminTranslatableFields(admin.ModelAdmin):
     form = TranslatableFieldsForm
-    search_fields = ('name', 'title', )
+    search_fields = ('name', 'title')
     list_display = ('content_type', 'name', 'title')
 
-    # fields = ['content_type', 'name', 'title']
-
-    fieldsets = (
-        (None, {
-            'fields': (
-                'content_type',
-                'name',
-                'title',
-            )
-        }),
-    )
+    fieldsets = ((None, {
+        'fields': (
+            'content_type',
+            'name',
+            'title',
+        ),
+    }), )
 
     autocomplete_fields = ('content_type', )
     url_name = '%s:%s_%s_autocomplete'
@@ -49,25 +44,17 @@ class AdminTranslatableFields(admin.ModelAdmin):
             obj.delete()
 
     def _changeform_view(self, request, object_id, form_url, extra_context):
-        # как беруться  фильд айди
-        id_ct = request.GET.get('id_ct') # class 'str'
-        print(id_ct)
-
+        id_ct = request.GET.get('id_ct')
         if id_ct:
-            # try:
-            ct = ContentType.objects.get_for_id(id_ct) # main | wine
-            print(ct)
-            model = ct.model_class() # class 'main.models.Wine'
-            print(4)
-            print(model)
-            print(model._meta.get_fields())
-            return JsonResponse({
-                'pk': ct.pk,
-                # как составляется урл? /ru/admin/main/wine/autocomplete/
-                'fields': [f.attname for f in model._meta.get_fields()],
-            })
-            # except Exception:
-            # return Http404('Error get content type')
+            try:
+                ct = ContentType.objects.get_for_id(id_ct)
+                model = ct.model_class()
+                return JsonResponse({
+                    'pk': ct.pk,
+                    'fields': [f.attname for f in model._meta.get_fields()],
+                })
+            except Exception:
+                return Http404('Error get content type')
         return super()._changeform_view(request, object_id, form_url, extra_context)
 
 
@@ -98,17 +85,8 @@ class AdminTranslations(admin.ModelAdmin):
         id_obj = request.GET.get('id_obj')
         if fld_id:
             try:
-                print(fld_id)
                 ct = TranslatableFields.objects.get(id=fld_id).content_type
-                print(ct)
-                print(ct.pk)
                 model = ct.model_class()
-                print(model)
-                print(model._meta.get_fields()) # (<django.db.models.fields.AutoField: id>, <django.db.models.fields.CharField: title>,
-                # /ru/admin/main/wine/autocomplete/
-                print(self.url_name % (self.admin_site.name, model._meta.app_label, model._meta.model_name))
-                print(reverse(self.url_name % (self.admin_site.name, model._meta.app_label, model._meta.model_name)))
-                print(str(model.objects.get(pk=id_obj)))
                 return JsonResponse({
                     'pk': ct.pk,
                     'url': reverse(self.url_name % (self.admin_site.name, model._meta.app_label, model._meta.model_name)),
--- tof/static/tof/js/translatable_fields_form.js
+++ tof/static/tof/js/translatable_fields_form.js
@@ -2,42 +2,29 @@
 * @Author: MaxST
 * @Date:   2019-11-09 13:52:25
 * @Last Modified by:   MaxST
-* @Last Modified time: 2019-11-12 21:09:24
+* @Last Modified time: 2019-11-17 14:37:55
 */
 (function ($) {
-
   window.generic_view_json = function (fields, text) {
     var $drop = $('#id_name');
     var $select = $drop;
     var value = $drop.val();
-    // var old_url = $drop.attr('data-ajax--url');
-
-    if (!$select.parent().is(".related-widget-wrapper")) {
+    if (! $select.parent().is(".related-widget-wrapper")) {
       $select = $('<div class="related-widget-wrapper"/>');
       $drop.replaceWith($select);
     } else {
       $select = $select.parent();
     }
-
     var sel = $('<select/>');
     sel.attr({
       'id': 'id_name',
       'name': 'name',
       'class': 'admin-autocomplete',
       'tabindex': -1,
-      // 'aria-hidden': true,
-      // 'data-ajax--cache': true,
-      // 'data-ajax--delay': 250,
-      // 'data-ajax--type': 'GET',
-      // 'data-ajax--url': '',
       'data-allow-clear': false,
       'data-placeholder': '',
       'data-theme': 'admin-autocomplete'
     });
-
-    // if (value && text) {
-    //   sel.append('<option value="'+value+'" >'+text+'</option>');
-    // }
     var data = [];
     $.each(fields, function (index, field) {
       var selected = '';
@@ -45,48 +32,32 @@
         selected = 'selected';
       }
       sel.append('<option value="' + field + '" ' + selected + '>' + field + '</option>');
-
-      data.push({id: index, text: field});
+      data.push({id: field, text: field});
     });
     $select.html(sel);
-    console.log(data);
-
     return $('#id_name').djangoAdminSelect2({
       ajax: {
-                data: function(params) {
-                    return {
-                        term: params.term,
-                        page: params.page
-                    };
-                }
-            }
+        processResults: function (get_data, page) {
+          return {results: data};
+        }
+      }
     });
   };
-
   $(document).ready(function () {
     $('#id_content_type').change();
-
     $('#id_content_type').change(function () {
       $('#id_name').removeAttr('readonly');
-
-      if (!$(this).val()) {
+      if (! $(this).val()) {
         $('#id_name').attr('readonly', true);
         return;
       }
       var params = {
-        'id_ct': $(this).val(),
+        'id_ct': $(this).val()
       };
-      // id_obj - текущий выбранный ответ
-      // if ($('#id_name').is('input')) {
-      //   params['id_obj'] = $('#id_name').val();
-      // }
       var esc = encodeURIComponent;
       $.get({
-        url: '?' + Object.keys(params).map(function (k) {
-          return esc(k) + '=' + esc(params[k]);
-        }).join('&'),
+        url: '?id_ct=' + $(this).val(),
         success: function (data, textStatus, jqXHR) {
-          $('#id_name').val(data.pk);
           generic_view_json(data.fields, data.text);
         },
         dataType: "json"
