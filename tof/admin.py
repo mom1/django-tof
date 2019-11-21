0a35fd8faea85a1e51e2667469ed7cd0b211d180
AlexeyRakitin <amrakitin@gmail.com>
2019-11-21 23:23:57 +0300

    limited selection

--- tof/admin.py
+++ tof/admin.py
@@ -7,6 +7,7 @@ import logging
 
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import CharField, TextField
 from django.http import JsonResponse
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -63,7 +64,7 @@ class TranslatableFieldsAdmin(admin.ModelAdmin):
                 model = ct.model_class()
                 return JsonResponse({
                     'pk': ct.pk,
-                    'fields': [f.attname for f in model._meta.get_fields()],
+                    'fields': [f.attname for f in model._meta.get_fields() if isinstance(f, (CharField, TextField))],
                 })
             except Exception as e:
                 logger.error(e)
