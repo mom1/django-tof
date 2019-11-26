5e34ff01c815843bcb27a648e531ff615d3129e1
MaxST <mstolpasov@gmail.com>
2019-11-26 11:42:43 +0300

    inline is not ready

--- example_project/main/admin.py
+++ example_project/main/admin.py
@@ -2,10 +2,8 @@
 # @Author: MaxST
 # @Date:   2019-10-28 20:30:42
 # @Last Modified by:   MaxST
-# @Last Modified time: 2019-11-19 15:46:22
+# @Last Modified time: 2019-11-26 11:42:17
 from django.contrib import admin
-
-from tof.admin import TranslationStackedInline, TranslationTabularInline
 from tof.decorators import tof_prefetch
 
 from .models import Vintage, Wine
@@ -15,7 +13,6 @@ from .models import Vintage, Wine
 class WineAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'active', 'sort')
     search_fields = ('title', )
-    inlines = (TranslationTabularInline, )
 
 
 @admin.register(Vintage)
